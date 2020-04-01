87f11814960ec957468b39554958a7e0fc2c2384
Ivan Arredondo <ivanac@gmail.com>
2020-03-31 18:57:50 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Whats up! Jo today is Tue.
         </p>
         <a
           className="App-link"
